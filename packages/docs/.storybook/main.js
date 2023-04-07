6020585835f130b3d13090d94bfee37871e2d859
Roger Fernandes <roger.afernandes@hotmail.com>
2023-04-07 16:47:22 -0300

    Ajusta subpath para Storybook de producao

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -17,5 +17,12 @@ const config = {
   docs: {
     autodocs: "tag",
   },
+  viteFinal: (config, { configType }) => {
+    if(configType === "PRODUCTION") {
+      config.base = "ignite-design-system/"
+    }
+
+    return config
+  },
 };
 export default config;
